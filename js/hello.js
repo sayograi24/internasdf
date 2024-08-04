5250eb7aa2212dea8e9558a75bac14602fb131ee
Sayog Rai <sayograi24@gmail.com>
2024-08-04 16:04:08 +0545

    updated

---
+++ js/hello.js
@@ -0,0 +1,15 @@
+function abc(){
+    document.getElementById('hello').innerText = "hello";
+    document.getElementById('yes').innerText= "Samit";
+    document.getElementById('no').innerText= "no";
+}
+function yes(){
+    document.getElementById('hello').innerText="adsf"
+    document.getElementById('yes').innerText="YESSS"
+    document.getElementById('no').innerText="no"
+}
+function no(){
+    document.getElementById('hello').innerText="adsf"
+    document.getElementById('yes').innerText="yes"
+    document.getElementById('no').innerText="NOOOOO"
+}
