6409f02613051f93258a9479b9cb1589b1ce2bf2
Sayog Rai <sayograi24@gmail.com>
2024-08-04 15:34:36 +0545

    update all

---
+++ js/index.js
@@ -0,0 +1,15 @@
+function abc() {
+    document.getElementById("demo").innerText = "asdf";
+    document.getElementById("yes").style.display = "block";
+    document.getElementById("no").style.display = "none";
+}
+
+function yes() {
+    document.getElementById("yes").style.display = "block";
+    document.getElementById("no").style.display = "none";
+}
+
+function no() {
+    document.getElementById("no").style.display = "block";
+    document.getElementById("yes").style.display = "none";
+}
